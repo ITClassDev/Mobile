f798b61fda92eeae4bbea1466f702ad6e52b82da
Slava Petrov <slavapmk@gmail.com>
2023-04-16 14:48:43 +0300

    Changed endpoint url to test server's ip

--- app/src/main/kotlin/ru/slavapmk/shtp/Values.kt
+++ app/src/main/kotlin/ru/slavapmk/shtp/Values.kt
@@ -6,7 +6,7 @@ import ru.slavapmk.shtp.io.ServerAPI
 import ru.slavapmk.shtp.io.dto.auth.User
 
 object Values {
-    private const val ENDPOINT_URL = "http://192.168.0.105:8080"
+    private const val ENDPOINT_URL = "http://91.203.192.42:8080"
     lateinit var token: String
     lateinit var user: User
 
