c30edd0fc60786101c61bf305321db057fea5e0d
Slava Petrov <slavapmk@gmail.com>
2023-04-21 23:45:33 +0300

    Fixed server errors at login page

--- app/src/main/kotlin/ru/slavapmk/shtp/ui/LoginActivity.kt
+++ app/src/main/kotlin/ru/slavapmk/shtp/ui/LoginActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
-import retrofit2.adapter.rxjava3.HttpException
+import retrofit2.HttpException
 import ru.slavapmk.shtp.Values
 import ru.slavapmk.shtp.databinding.ActivityLoginBinding
 import ru.slavapmk.shtp.io.dto.auth.AuthLoginRequest
@@ -33,67 +33,74 @@ class LoginActivity : AppCompatActivity() {
                 .subscribe { me ->
                     Values.user = me.user
                     val myIntent = Intent(this@LoginActivity, MainActivity::class.java)
-                    myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    myIntent.flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(myIntent)
                 }
             return
         }
 
         binding.loginLayout.visibility = View.VISIBLE
-        binding.loginButton.visibility = View.VISIBLE
-        binding.statusProgress.visibility = View.GONE
+        binding.buttonLogin.visibility = View.VISIBLE
+        binding.statusProgressBar.visibility = View.GONE
 
-        binding.loginInput.onFocusChangeListener = View.OnFocusChangeListener { _, _ ->
+        binding.inputEmail.onFocusChangeListener = View.OnFocusChangeListener { _, _ ->
             hideErrors()
         }
-        binding.passwordInput.onFocusChangeListener = View.OnFocusChangeListener { _, _ ->
+        binding.inputPassword.onFocusChangeListener = View.OnFocusChangeListener { _, _ ->
             hideErrors()
         }
-    }
+        binding.buttonLogin.setOnClickListener {
+            hideErrors()
+            binding.statusProgressBar.visibility = View.VISIBLE
 
-    @SuppressLint("CheckResult")
-    fun login(view: View?) {
-        hideErrors()
-        binding.statusProgress.visibility = View.VISIBLE
-
-        Values.api.login(
-            AuthLoginRequest(
-                binding.loginInput.text.toString(), binding.passwordInput.text.toString()
+            Values.api.login(
+                AuthLoginRequest(
+                    binding.inputEmail.text.toString(), binding.inputPassword.text.toString()
+                )
             )
-        )
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe({ loginResponse ->
-                Values.token = "Bearer ${loginResponse.accessToken}"
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ loginResponse ->
+                    Values.token = "Bearer ${loginResponse.accessToken}"
 
-                Values.api.getMe(Values.token)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeOn(Schedulers.io())
-                    .subscribe { me ->
-                        Values.user = me.user
-                        getSharedPreferences(Values.APP_ID, MODE_PRIVATE).edit {
-                            this.putString(Values.AUTH_ID, Values.token).apply()
+                    Values.api.getMe(Values.token)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe { me ->
+                            Values.user = me.user
+                            getSharedPreferences(Values.APP_ID, MODE_PRIVATE).edit {
+                                this.putString(Values.AUTH_ID, Values.token).apply()
+                            }
+                            val myIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                            myIntent.flags =
+                                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(myIntent)
                         }
-                        val myIntent = Intent(this@LoginActivity, MainActivity::class.java)
-                        myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        startActivity(myIntent)
+                }, {
+                    when (it) {
+                        is ConnectException -> binding.messageNoServerAccess.visibility =
+                            View.VISIBLE
+
+                        is HttpException -> when (it.code()) {
+                            401 ->
+                                binding.messageIncorrectCredentials.visibility = View.VISIBLE
+
+                            422 ->
+                                binding.messageIncorrectInput.visibility = View.VISIBLE
+                        }
+
+                        else -> binding.messageNetworkError.visibility = View.VISIBLE
                     }
-            }, {
-                if (it is ConnectException)
-                    binding.noInternet.visibility = View.VISIBLE
-                else if (it is HttpException && it.code() == 422)
-                    binding.incorrectDataStatus.visibility = View.VISIBLE
-                else
-                    binding.networkError.visibility = View.VISIBLE
-                binding.statusProgress.visibility = View.GONE
-            })
-
-
+                    binding.statusProgressBar.visibility = View.GONE
+                })
+        }
     }
 
     private fun hideErrors() {
-        binding.incorrectDataStatus.visibility = View.GONE
-        binding.noInternet.visibility = View.GONE
-        binding.networkError.visibility = View.GONE
+        binding.messageIncorrectCredentials.visibility = View.GONE
+        binding.messageNoServerAccess.visibility = View.GONE
+        binding.messageNetworkError.visibility = View.GONE
+        binding.messageIncorrectInput.visibility = View.GONE
     }
 }
