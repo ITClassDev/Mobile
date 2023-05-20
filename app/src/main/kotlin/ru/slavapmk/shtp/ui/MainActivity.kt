11ef98dd2f6ca0f5655b997afab650877284c9c8
Slava Petrov <slavapmk@gmail.com>
2023-05-20 12:45:57 +0300

    Added new version alert dialog

--- app/src/main/kotlin/ru/slavapmk/shtp/ui/MainActivity.kt
+++ app/src/main/kotlin/ru/slavapmk/shtp/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+import ru.slavapmk.shtp.BuildConfig
 import ru.slavapmk.shtp.R
 import ru.slavapmk.shtp.Values
 import ru.slavapmk.shtp.databinding.ActivityMainBinding
@@ -37,12 +38,19 @@ class MainActivity : AppCompatActivity() {
         Values.versionManager.lastVersion
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
-            .subscribe {
-//                if (BuildConfig.VERSION_CODE < it.code)
-//                    Toast
-//                        .makeText(this, "New version ${it.name} available", Toast.LENGTH_SHORT)
-//                        .show()
-            }
+            .subscribe({
+                if (BuildConfig.VERSION_CODE < it.code) {
+                    val dialog = NewVersionDialog()
+                    val manager: FragmentManager = supportFragmentManager
+                    val transaction = manager.beginTransaction()
+                    val args = Bundle()
+                    args.putString("version_name", it.name)
+                    args.putInt("version_code", it.code)
+                    args.putString("version_url", it.downloadUrl)
+                    dialog.arguments = args
+                    dialog.show(transaction, "new_version_dialog")
+                }
+            }, {})
     }
 
 }
