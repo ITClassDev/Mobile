8803637317a5bb1685eed46228e6ab3ec6dff17a
Slava Petrov <slavapmk@gmail.com>
2023-09-07 20:39:21 +0300

    Changed all endpoints and removed all slashes

--- app/src/main/kotlin/ru/slavapmk/shtp/io/ServerAPI.kt
+++ app/src/main/kotlin/ru/slavapmk/shtp/io/ServerAPI.kt
@@ -29,53 +29,53 @@ import ru.slavapmk.shtp.io.dto.user.patch.PatchUserRequest
 import ru.slavapmk.shtp.io.dto.user.put.UserPut
 
 interface ServerAPI {
-    @POST("auth/login/")
+    @POST("auth/login")
     fun login(
         @Body authRequest: AuthLoginRequest
     ): Observable<AuthLoginResponse>
 
-    @GET("auth/me/")
+    @GET("auth/me")
     fun getMe(
         @Header("Authorization") token: String
     ): Observable<AuthMeResponse>
 
-    @PATCH("users/")
+    @PATCH("users")
     fun updateProfile(
         @Header("Authorization") token: String,
         @Body user: PatchUserRequest
     ): Observable<PatchResult>
 
-    @GET("users/")
+    @GET("users")
     fun allUsers(
         @Header("Authorization") token: String
     ): Observable<UsersAll>
 
-    @PUT("users/")
+    @PUT("users")
     fun registerUser(
         @Header("Authorization") token: String,
         @Body user: UserPut
     ): Completable
 
-    @PUT("users/groups/")
+    @PUT("users/groups")
     fun createGroup(
         @Header("Authorization") token: String,
         @Body group: GroupPut
     ): Completable
 
-    @DELETE("users/groups/{group_id}/")
+    @DELETE("users/groups/{group_id}")
     fun deleteGroup(
         @Header("Authorization") token: String,
         @Path("group_id") groupId: Int
     ): Completable
 
-    @PATCH("users/avatar/")
+    @PATCH("users/avatar")
     @Multipart
     fun updateAvatar(
         @Header("Authorization") token: String,
         @Part file: MultipartBody.Part
     ): Observable<PatchAvatarResponse>
 
-    @PUT("achievements/")
+    @PUT("achievements")
     @Multipart
     fun putAchievement(
         @Header("Authorization") token: String,
@@ -83,28 +83,28 @@ interface ServerAPI {
         @Part file: MultipartBody.Part
     ): Observable<PatchAvatarResponse>
 
-    @DELETE("users/{user_id}/")
+    @DELETE("users/{user_id}")
     fun deleteUser(
         @Header("Authorization") token: String,
         @Path("user_id") userId: Int
     ): Completable
 
-    @GET("achievements/")
+    @GET("achievements")
     fun getAchievements(@Header("Authorization") token: String): Observable<AchievementsResponse>
 
-    @GET("users/my_notifications/")
+    @GET("users/my_notifications")
     fun getNotifications(@Header("Authorization") token: String): Observable<AllNotifications>
 
-    @PUT("notifications/")
+    @PUT("notifications")
     fun sendNotification(
         @Header("Authorization") token: String,
         @Body notification: PostNotification
     ): Completable
 
-    @GET("users/get_leaderboard/")
+    @GET("users/get_leaderboard")
     fun getLeaderBoard(): Observable<LeaderBoard>
 
-//    @GET("programming/day_challenge/")
+//    @GET("programming/day_challenge")
 //    fun getDailyChallenge(@Header("Authorization") token: String): Observable<DailyChallenge>
 
 }
--- release.md
+++ release.md
@@ -1,3 +1,3 @@
 Temporary update is a stub due to the transition of the BackEnd to a new version, with the possibility of supporting the old one. The next update will be for the transition to Api v2
 
-Changed the endpoint with the version postscript
+Changed all method endpoints to new schema
